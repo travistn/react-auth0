cb9cf60b620ccca6ee441fc6a2dd637f6db9afe3
travistn <travisn714@gmail.com>
2023-02-08 13:41:12 -0800

    Added Auth0Provider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './App';
 import './index.css';
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
